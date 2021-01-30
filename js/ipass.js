15d9da3db59706472f956ad3b71dd5161e45a4e9
nounix <martin.muenning@gmail.com>
2021-01-30 16:20:23 +0100

    fix sec bug, refactor

--- js/ipass.js
+++ js/ipass.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
 
         event.preventDefault()
 
-        masterPwd = btoa(keccak512.digest(event.data.input.val()))
+        masterPwd = sha3_512(event.data.input.val())
         event.data.input.val('')
         $('#locked-view').removeClass("d-block");
         $('#locked-view').addClass("d-none");
@@ -99,7 +99,7 @@ $(document).ready(function () {
         let config = JSON.parse(localStorage.getItem(account))
         let len = config?.len || 32
         let version = config?.version || 1
-        let special = config?.special || true
+        let special = (config?.special === undefined) ? true : config?.special
 
         return genPwd(masterPwd + account + version, len, special)
     }
@@ -111,14 +111,16 @@ $(document).ready(function () {
             return table
         }
 
-        function syncMap(t1, t2, t3) {
-            t1 = [...t1];
-            let xn = t1.length - t3.length
+        function syncMap(tuRef, trRef, tval) {
+            let tu = [...tuRef];
+            let tr = [...trRef];
 
-            t1 = t1.slice(xn / 2, t1.length - xn / 2)
-            t2 = t2.filter(e => t1.includes(e))
+            let lenDiff = tu.length - tval.length
 
-            return t2.map(num => t3[t1.indexOf(num)])
+            tu = tu.slice(lenDiff / 2, tu.length - lenDiff / 2)
+            tr = tr.filter(e => tu.includes(e))
+
+            return tr.map(num => tval[tu.indexOf(num)])
         }
 
         let numbers = tableFromCharCodeRange(48, 57)
@@ -128,14 +130,8 @@ $(document).ready(function () {
         let allowedChars = [numbers, capitalLetters, smallLetters]
         if (special) allowedChars.push(specialChars)
 
-        // Generate base64 encoded sha3 hash
-        let hash = btoa(keccak512.digest(input))
-
-        // Generate random numbers from hash
-        let tr = [];
-        for (let i = 0; i < hash.length; i++) {
-            tr.push(hash.charCodeAt(i) + hash.charCodeAt(i + 1) + hash.charCodeAt(i + 2));
-        }
+        // Generate random numbers from hash and hash of hash
+        let tr = [...sha3_512.digest(input), ...sha3_512.digest(sha3_512.digest(input))]
 
         // Generate sorted and unique numbers from random numbers table
         let tu = [...new Set(tr.filter(n => n).sort((a, b) => a - b))];
@@ -145,14 +141,10 @@ $(document).ready(function () {
 
         // Check and mod password until it has at least one of each allowed char types
         let counter = 0;
-        while (!allowedChars.every(a => a.some(e => pwd.includes(e)))) {
-            allowedChars.forEach(e => {
-                if (!e.some(num => pwd.join('').includes(num))) {
-                    let nindex = syncMap(tu, tr, e)[counter]
-                    let pindex = syncMap(tu, tr, [...Array(pwd.length).keys()])[counter]
-
-                    pwd[pindex] = nindex
-
+        while (!allowedChars.every(tc => tc.some(c => pwd.includes(c)))) {
+            allowedChars.forEach(tc => {
+                if (!tc.some(c => pwd.includes(c))) {
+                    pwd[syncMap(tu, tr, [...Array(pwd.length).keys()])[counter]] = syncMap(tu, tr, tc)[counter]
                     counter++;
                 }
             })
