a2ac7f245119e94b1a75c57979d659be4257cc35
nounix <martin.muenning@gmail.com>
2021-03-14 19:27:24 +0100

    up

--- sw.js
+++ sw.js
@@ -12,37 +12,16 @@ self.addEventListener('message', (event) => {
   }
 });
 
-// self.addEventListener('fetch', function (e) {
-//   caches.open(CACHE_VERSION).then((cache) => cache.add(e.request.url))
-//   e.respondWith(
-//     fetch(e.request).catch(function(err) {
-//       // console.error('Fetch failed; returning offline page instead.', err);
-//       return caches.open(CACHE_VERSION).then(function(cache) {
-//         return cache.match(e.request.url);
-//       });
-//     }))
-// });
-
-self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.open(CACHE_VERSION).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
+    caches.open(CACHE_VERSION).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
         if (response) return response;
 
-        return fetch(event.request.clone()).then(function(response) {
+        return fetch(event.request.clone()).then(function (response) {
           if (response.status < 400) cache.put(event.request, response.clone());
           return response;
         });
-      }).catch(function(error) {
-        // This catch() will handle exceptions that arise from the match() or fetch() operations.
-        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
-        // It will return a normal response object that has the appropriate error code set.
-        console.error('  Error in fetch handler:', error);
-        console.log("event = ", event)
-
-        throw error;
       });
     })
   );
